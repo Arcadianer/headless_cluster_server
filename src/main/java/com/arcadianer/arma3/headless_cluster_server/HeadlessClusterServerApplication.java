7a7b12609e396107c57d341493ea59ef5d4b444b
Manuel <Mplonski@t-online.de>
2018-05-23 11:53:54 +0200

    Initial commit

---
+++ src/main/java/com/arcadianer/arma3/headless_cluster_server/HeadlessClusterServerApplication.java
@@ -0,0 +1,12 @@
+package com.arcadianer.arma3.headless_cluster_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessClusterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlessClusterServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/arcadianer/arma3/headless_cluster_server/HeadlessClusterServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arcadianer.arma3.headless_cluster_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeadlessClusterServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
